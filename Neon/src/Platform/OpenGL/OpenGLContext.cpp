df00b7b71f881c8dcddaa8fc3551d38d4838b15b
Guillaume Dévaud <gdvaud@gmail.com>
2020-12-02 21:38:14 +0100

    Check OpenGL version on startup

--- Neon/src/Platform/OpenGL/OpenGLContext.cpp
+++ Neon/src/Platform/OpenGL/OpenGLContext.cpp
@@ -1,17 +1,27 @@
 #include "OpenGLContext.h"
 
 namespace Neon {
-    OpenGLContext::OpenGLContext(GLFWwindow* windowHandle)
-        : m_WindowHandle(windowHandle) {
+    OpenGLContext::OpenGLContext(GLFWwindow* windowHandle) : m_WindowHandle(windowHandle) {
         NEO_CORE_ASSERT(windowHandle, "Window handle is null");
     }
     void OpenGLContext::Init() {
         glfwMakeContextCurrent(m_WindowHandle);
         int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
         NEO_CORE_ASSERT(status, "Failed to initialize Glad");
+
+        NEO_CORE_INFO("OpenGL Info:");
+        NEO_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+        NEO_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+        NEO_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
+
+#ifdef NEO_ENABLE_ASSERTS
+        int versionMajor, versionMinor;
+        glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+        glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+        NEO_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5),
+                        "Neon requires OpenGL version 4.5 or higher");
+#endif
     }
 
-    void OpenGLContext::SwapBuffers() {
-        glfwSwapBuffers(m_WindowHandle);
-    }
+    void OpenGLContext::SwapBuffers() { glfwSwapBuffers(m_WindowHandle); }
 }  // namespace Neon
